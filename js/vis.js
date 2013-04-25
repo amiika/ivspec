dfa6c3857a885a16b98815d92295d20459889714
amiika <amiika@gmail.com>
2013-04-25 17:19:19 +0300

    updated

--- js/vis.js
+++ js/vis.js
@@ -7,6 +7,7 @@ vis.config(['$httpProvider', function($httpProvider) {
 
 
 function visCtrl($scope, SparqlService, PrefixService,VocabService) {
+	$scope.show = true;
 	$scope.classes = undefined;
 	$scope.propsFound = false;
 	$scope.validEndpoint = true;
@@ -90,8 +91,10 @@ function visCtrl($scope, SparqlService, PrefixService,VocabService) {
     
    function getProps(graph) {
     	$scope.propsFound=false;
+    	$scope.querying = true; 
         SparqlService.query($scope.endpoint,proQry(graph))
          .success(function(data) {
+         	$scope.querying = false; 
          	$scope.propsFound=true;
        		propsInUse = [];
 			
@@ -129,9 +132,10 @@ function ValidUrl(str) {
 }
     	
     function getClasses(endpoint,graph,type,filter) {
-        
+        $scope.querying = true; 
          SparqlService.query(endpoint,classQry(graph,type.id,filter))
          .success(function(data) {
+         	$scope.querying = false; 
             data = data.results.bindings;
             var namespaces = [];
             var classesInUse = [];
@@ -194,6 +198,7 @@ vis.directive('ghVisualization', function () {
     restrict: 'E',
     scope: {
       val: '=',
+      loading: '=',
       grouped: '='
     },
     link: function (scope, element, attrs) {
@@ -211,7 +216,12 @@ vis.directive('ghVisualization', function () {
           .call(d3.behavior.zoom().on("zoom", redraw))
           .append('g');
           
-
+			var rect = svg.append('rect')
+	    		.attr('width', width*50)
+			    .attr('x',-width/2*50)
+	    		.attr('y',-height/2*50)
+	    		.attr('height', height*50)
+	    		.attr('fill', 'white');
     
 		function redraw() {
 		  svg.attr("transform",
@@ -219,89 +229,73 @@ vis.directive('ghVisualization', function () {
 		      + " scale(" + d3.event.scale + ")");
 		}
     	
+    	scope.$watch('loading',function(load) {
+    		
+    		if(load) {
+    		svg.selectAll('.node').remove();
+    		
+			var lineTotal = 6,
+			  lineLength = 40,
+			  speed = 1,
+			  width = 50,
+			  radius = 40,
+			  color = '#333',
+			
+			  posX = width*10,
+			  posY = height/2,
+			  rotIncrement = Math.floor(360/lineTotal),
+			  group,
+			  lines;
+			 
+			function data() {
+			  return new Array(lineTotal);
+			}
+			 
+			// <g> only needed for nice display in tributary
+			group = svg.append('g')
+			.attr('class',"node")
+				.attr('transform', 'translate(' + [posX, posY] + ')');
+			
+			lines = group.selectAll('line')
+			.data(data)
+			.enter()
+			.append('line')
+			  .attr({
+			    'fill': 'none',
+			    'stroke-linecap': 'round',
+			    'stroke-width': width,
+			    'x1': radius,
+			    'y1': radius,
+			    'x2': lineLength,
+			    'y2': lineLength,
+			    'stroke': color,
+			    'transform': function (d,i) {
+			      return 'rotate(' + (i + 1) * rotIncrement + ')';
+			    }
+			  })
+			.append('animate')
+			  .attr({    
+			    'attributeName': 'opacity',
+			    'repeatCount': 'indefinite',
+			    'attributeType': 'CSS',
+			    'from': 1,
+			    'to': 0.3,
+			    'dur': speed + 's',
+			    'begin': function (d,i) {
+			      return 16/150 * (i + 1) + 's';
+			    }
+			  });
+    		} else {
+    			svg.selectAll('.node').remove();
+    		}
+    		
+    	});
+    	
     	scope.$watch('val',function(data) {
     		if(data!==undefined) {
-    	    console.log("Visualized data:");
-    		console.log(data);
-    		
-    		svg.selectAll('*').remove();
-    		
-    		
-			var n = 200,
-			    m = 10,
-			    padding = 6;
-			    //color = d3.scale.ordinal()
-    			//.domain(["6TH", "7TH", "5TH", "4TH"])
-    			//.range(colorbrewer.RdBu[4]);
-
-      				
-      			//var color = d3.scale.linear()
-    			//.domain([0, d3.max(_.map(data, function(d) { return d.count; }))])
-    			//.range(["blue", "green"]);
     			
-    			var color = d3.scale.quantize()
-    			.domain([0,d3.max(_.map(data, function(d) { return d.count; }))])
-    			.range(["#FFA300", "#FF661A", "#75BA23","#7E69AA","#009577" ]); 
-
-
-			var nodes = data;
-			
-			/*
-			var nodes = d3.range(n).map(function() {
-			  var i = Math.floor(Math.random() * m),
-			      v = (i + 1) / m * -Math.log(Math.random());
-			  return {
-			    r: r(v),
-			    color: color(i)
-			  };
-			});*/
-			
-			var force = d3.layout.force()
-			    .nodes(nodes)
-			    .size([width, height])
-			    .gravity(0)
-			    .charge(0)
-			    .on("tick", tick)
-			    .start();
-			    			
-			svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-			
-			svg.append('rect')
-	    		.attr('width', width)
-	    		.attr('height', height)
-	    		.attr('fill', 'white')
-
-			
-			svg.selectAll("g .node").data(nodes);
-			 
-			var g = svg.selectAll("g .node")
-					.data(nodes)
-					.enter()
-					.append("g")
-			 		.attr("class", "node")
-      				.call(force.drag);
-			 			
-			var circle = g.append("circle")
-			    		.attr("r", function(d) { return d.r; })
-			    		.style("fill", function(d) { return color(d.count); });
-			    			
-			g.append("text")
-      		.attr("class","text")
-      		.text(function(d) { return d.label!==undefined ? d.label : d.prefixed })
-      		.style("font-size", function(d) { return 2*d.r / this.getComputedTextLength() * 9 + "px"; })
-      		.attr("text-anchor", "middle")
-      		.attr("dy", ".2em")
-      		
-      		g.append("text")
-      		.attr("class","text")
-      		.text(function(d) { return d.count })
-      		.style("font-size", function(d) { return Math.max(d.r / this.getComputedTextLength() * 6 + "px",20); })
-      		.attr("text-anchor", "bottom")
-      		.attr("text-anchor", "middle")
-      		.attr("dy", "1.3em");      		 
-      		 
-			function tick(e) {
+    			
+    					function tick(e) {
 			  	g.each(cluster(10 * e.alpha * e.alpha))
 			     .each(collide(.1))
 			     .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
@@ -379,7 +373,85 @@ vis.directive('ghVisualization', function () {
 			          || y2 < ny1;
 			    });
 			  };
-			}
+			}	
+    			
+    	    console.log("Visualized data:");
+    		console.log(data);
+    		
+    		svg.selectAll('.node').remove();
+    		
+    		
+			var n = 200,
+			    m = 10,
+			    padding = 6;
+			    //color = d3.scale.ordinal()
+    			//.domain(["6TH", "7TH", "5TH", "4TH"])
+    			//.range(colorbrewer.RdBu[4]);
+
+      				
+      			//var color = d3.scale.linear()
+    			//.domain([0, d3.max(_.map(data, function(d) { return d.count; }))])
+    			//.range(["blue", "green"]);
+    			
+    			var color = d3.scale.quantize()
+    			.domain([0,d3.max(_.map(data, function(d) { return d.count; }))])
+    			.range(["#FFA300", "#FF661A", "#75BA23","#7E69AA","#009577" ]); 
+
+
+			var nodes = data;
+			/*
+			var nodes = d3.range(n).map(function() {
+			  var i = Math.floor(Math.random() * m),
+			      v = (i + 1) / m * -Math.log(Math.random());
+			  return {
+			    r: r(v),
+			    color: color(i)
+			  };
+			});*/
+			
+			var force = d3.layout.force()
+			    .nodes(nodes)
+			    .size([width, height])
+			    .gravity(0)
+			    .charge(0)
+			    .on("tick", tick)
+			    .start();
+			    			
+			svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+			
+
+
+			
+			svg.selectAll("g .node").data(nodes);
+			 
+			var g = svg.selectAll("g .node")
+					.data(nodes)
+					.enter()
+					.append("g")
+			 		.attr("class", "node")
+      				.call(force.drag);
+			 			
+			var circle = g.append("circle")
+			    		.attr("r", function(d) { return d.r; })
+			    		.style("fill", function(d) { return color(d.count); });
+			    			
+			g.append("text")
+      		.attr("class","text")
+      		.text(function(d) { return d.label!==undefined ? d.label : d.prefixed })
+      		.style("font-size", function(d) { return 2*d.r / this.getComputedTextLength() * 9 + "px"; })
+      		.attr("text-anchor", "middle")
+      		.attr("dy", ".2em")
+      		
+      		g.append("text")
+      		.attr("class","text")
+      		.text(function(d) { return d.count })
+      		.style("font-size", function(d) { return Math.max(d.r / this.getComputedTextLength() * 6 + "px",20); })
+      		.attr("text-anchor", "bottom")
+      		.attr("text-anchor", "middle")
+      		.attr("dy", "1.3em");      		 
+      		 
+
     		
     	}	
     		
